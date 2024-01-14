b342051e0651dbc8df377d780921bed834b97f4b
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-01-14 19:51:49 +0530

    Create  printingVar.java
    printing value of variable

---
+++ printingVar.java
@@ -0,0 +1,8 @@
+public class printingVar {
+    public static void main (String args []){
+        String name = "Devanshu" ;
+        int age = 20 ;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
