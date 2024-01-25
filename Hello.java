10d928b739b4d98c8eb09909f78227393e5954c9
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-01-25 11:37:42 +0530

    Create Hello.java
    //a program to print Hello world 10 times using do while loop

---
+++ Hello.java
@@ -0,0 +1,13 @@
+//a program to print Hello world 10 times using do while loop
+public class hello100{
+    public static void main (String args []){
+        int counter = 0;
+        while (counter <10){
+            System.out.println("Hello World" );
+            counter ++;
+           
+        }
+        
+    }
+
+}
