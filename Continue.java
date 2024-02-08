7297c739ad4728cf746b279fda36cd0d75b5cb10
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-02-08 23:04:35 +0530

    Create Continue.java
    //a program to display example of continue

---
+++ Continue.java
@@ -0,0 +1,15 @@
+//a program to display example of continue
+
+public class continuee {
+    public static void main (String args []){
+         
+         for (int i =0 ; i<= 10 ; i++){
+            if (i == 3 || i == 6){
+                continue;
+            }
+            System.out.println(i);
+         }
+    }
+    
+    
+}
