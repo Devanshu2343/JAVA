4cd546ad1e1ad3c7669d31f9ddcb8b7fbd7115dd
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-02-19 22:30:51 +0530

    Create Exponent_in_Double.java

---
+++ Exponent_in_Double.java
@@ -0,0 +1,25 @@
+/* 
+ * FOR EXAMPLE :
+ * float f1 = 35e3f
+ * here
+ *    x = 35
+ *    y = 3
+ *   f1 = 35 * (10)^3
+ *  e represents x * (10)^y
+*/
+
+
+
+import java.util.* ;
+public class exp_in_double {
+    public static void main (String args []){
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+
+        System.out.println(f1);
+        System.out.println(d1);
+
+    }
+    
+}
