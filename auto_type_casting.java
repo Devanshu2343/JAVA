50794e7d8cb64d112ae200b0c3b3deb46ed364f7
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-02-22 21:10:56 +0530

    Create auto_type_casting.java
    A PROGRAM IN JAVA TO SHOW AUTO TYPE-CASTING OF INTEGER TO DOUBLE

---
+++ auto_type_casting.java
@@ -0,0 +1,15 @@
+//A PROGRAM IN JAVA TO SHOW AUTO TYPE-CASTING OF INTEGER TO DOUBLE
+
+import java.util.*;
+public class auto_type_casting {
+    public static void main(String[] args) {
+        System.out.println ("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int myInt = sc.nextInt();
+        double myDouble = myInt; // Automatic casting: int to double
+    
+        System.out.println(myInt);      // Outputs = myInt
+        System.out.println(myDouble);   // Outputs in float 
+      }
+    
+}
