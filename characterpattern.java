be6eb4c5816c79785330cd7b26a2543c45ce12ce
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-02-24 23:18:28 +0530

    Create characterpattern.java
    Character Pattern

---
+++ characterpattern.java
@@ -0,0 +1,22 @@
+/*
+ * A
+ * BC
+ * DEF
+ * GHIJ
+ */
+
+public class charpattern {
+    public static void main (String args []){
+        char k='A';
+        for (int i =1 ; i <=4 ;i++){
+            for (int j = 1 ; j<=i ; j++){
+                System.out.print(k);
+                k ++ ;
+            } 
+            System.out.println();
+        }
+
+    }
+
+    
+}
