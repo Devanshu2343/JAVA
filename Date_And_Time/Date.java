04b4b12a05e4fab4078534ee4dcaea1e451f57a3
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-04-10 12:51:20 +0530

    Create Date.java
    A program in java to display current date

---
+++ Date_And_Time/Date.java
@@ -0,0 +1,11 @@
+//A program in java to display current date
+import java.time.LocalDate;
+
+public class Date {
+  public static void main(String[] args) {
+    LocalDate tod_date = LocalDate.now(); 
+    
+    System.out.println(tod_date);
+    
+  }
+}
