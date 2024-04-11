9e3b142f5134a70a942129d549a510caf440c27b
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-04-11 20:29:53 +0530

    Create Time.java

---
+++ Date_And_Time/Time.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Time {  
+  public static void main(String[] args) { 
+
+    LocalTime curr = LocalTime.now();
+
+    System.out.println("Current Time :" + curr);
+  }  
+}  
