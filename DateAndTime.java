6a9f378b3d6f9088b5d31dae7853da173de95565
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-04-15 13:47:58 +0530

    Create DateAndTime.java

---
+++ DateAndTime.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime; 
+
+public class DateAndTime {
+  public static void main(String[] args) {
+    LocalDateTime curr = LocalDateTime.now();
+    System.out.println(curr);
+  }
+}
