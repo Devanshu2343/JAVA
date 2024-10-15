42be61d86017ca52d7ad28883185b896d2c5d6fe
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-10-16 00:15:49 +0530

    Create DateAndTime1.java
    A program in java to display current date and time

---
+++ Date_And_Time/DateAndTime1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class DateAndTime{
+
+   public static void main(String args[]) {
+      
+      Date date = new Date();
+
+      
+      System.out.println(date.toString());
+   }
+}
