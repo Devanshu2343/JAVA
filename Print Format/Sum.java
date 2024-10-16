61c9db8127d2a1da1d1c9a398df5162f013403a1
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-10-16 23:30:54 +0530

    Create Sum.java
    use of printf function

---
+++ Print Format/Sum.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) {
+        float a = 556.6665f; 
+        float b = 455.980f; 
+        float c = a + b;
+
+       
+        System.out.printf("%.2f%n", c); 
+    }
+}
